6712afb1ed57e52a6efca056fb246bbe5b8e3613
woobottle <dkrnfls@gmail.com>
2020-12-11 23:00:11 +0900

    add python algorithm

---
+++ BaekjoonAlgorithm/Python/1731.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+nrray = [0] * n
+
+for i in range(n) :
+  nrray[i] = int(sys.stdin.readline())
+
+if(nrray[n-1] - nrray[n-2] == nrray[n-2] - nrray[n-3]) :
+  print(nrray[n-1] + nrray[1] - nrray[0])
+else :
+  print(int(nrray[n-1] * nrray[1] / nrray[0]))
