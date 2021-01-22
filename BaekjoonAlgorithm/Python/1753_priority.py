0ff18925a668379e89a3fd04eeb139046d991b6e
woobottle <dkrnfls@gmail.com>
2021-01-22 22:36:51 +0900

    add python algorith

--- BaekjoonAlgorithm/Python/1753_priority.py
+++ BaekjoonAlgorithm/Python/1753_priority.py
@@ -1,36 +1,33 @@
 # 1753번 우선순위 큐 사용
 import heapq
+import sys 
 
 INF = int(1e9)
-v,e = map(int, input().split())
-k = int(input())
+v,e = map(int, sys.stdin.readline().split())
+k = int(sys.stdin.readline())
 graph = [[] for _ in range(v+1)]
-# visited = [False] * (v+1)
 distance = [INF] * (v+1)
 
 for _ in range(e) :
-  u,v,w = map(int,input().split())
-  graph[u].append((v,w))
+  u,v,w = map(int,sys.stdin.readline().split())
+  graph[u].append([v,w])
   
 def dijkstra(start) :
   q = []
-  heapq.heappush(q, (0, start))
+  heapq.heappush(q, [0, start])
   distance[start] = 0
   while q :
     dist, now = heapq.heappop(q)
     if distance[now] < dist :
       continue
-    for i in graph[now] :
-      cost = dist + i[1]
-      if cost < distance[i[0]] :
-        distance[i[0]] = cost
-        heapq.heappush(q, (i[1], i[0]))
+    for target, weight in graph[now] :
+      cost = dist + weight
+      if cost < distance[target] :
+        distance[target] = cost
+        heapq.heappush(q, (cost, target))
 
 
 dijkstra(k)
 
-for i in range(1, v+2) :
-  if(distance[i] == INF) :
-    print("INF") 
-  else :
-    print(distance[i])
+for i in distance[1:] :
+  print(i if i != INF else "INF")
