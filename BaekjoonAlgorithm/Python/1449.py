52d9bade2be983be89f0f0b3a4a4e3403641ddc5
woobottle <dkrnfls@gmail.com>
2021-01-30 15:18:55 +0900

    add python algorithm

--- BaekjoonAlgorithm/Python/1449.py
+++ BaekjoonAlgorithm/Python/1449.py
@@ -1,5 +1,24 @@
-n, l = map(int, input().split())
-holes = list(map(int,input().split())).sort()
+global L 
+N, L = map(int, input().split())
+holes = sorted(list(map(int,input().split())))
+
+start = 0
+tape_count = 0
+
+def calc_start(h) :
+  i = 0
+  interval = 0
+  while i < len(h) - 1 :
+    if interval + (h[i+1] - h[i]) < L :
+      interval += (h[i+1] - h[i])
+      i += 1
+    else : 
+      break
+  return i
+
+while start < len(holes) :
+  start = start + calc_start(holes[start:]) + 1
+  tape_count += 1
 
 
-
+print(tape_count)
