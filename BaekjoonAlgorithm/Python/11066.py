90658f047cdf06ad1657aa565b3193eaa5b3111e
woobottle <dkrnfls@gmail.com>
2021-03-20 23:03:13 +0900

    add python algorithm

--- BaekjoonAlgorithm/Python/11066.py
+++ BaekjoonAlgorithm/Python/11066.py
@@ -1,6 +1,17 @@
 import sys
+import heapq
+
 t = int(sys.stdin.readline().strip())
+
 for _ in range(t) :
   k = int(sys.stdin.readline().strip())
-  l = sorted(list(map(int, sys.stdin.readline().strip().split())))
-  print(l)
+  l = sorted(list(map(int, sys.stdin.readline().strip().split())), reverse=True)
+  answer = 0
+  while len(l) != 2 :
+    first = l.pop()
+    second = l.pop()
+    temp = first + second
+    answer += temp
+    l.insert(0, temp)
+  answer += sum(l)
+  print(answer)
